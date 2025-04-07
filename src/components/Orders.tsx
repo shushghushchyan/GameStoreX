2a83b02739493d6fcbbdcf09880a226f4e342b7b
Shushanna <shushannaghushchyan20@gmail.com>
2025-04-08 00:19:51 +0400

    orders also is ready

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,12 +1,13 @@
 import Header from './Header';
 import arrow from '../assets/Logos/arrow.svg';
+import success from '../assets/Logos/success.svg';
 
 
 const ordersArray = [
   {
     transactionId: "15325",
     date: "2025-04-06",
-    status: "Completed",
+    status: "Success",
     gameName: "Game A",
     gameId: "GA001",
     amount: "$20.00"
@@ -14,7 +15,7 @@ const ordersArray = [
   {
     transactionId: "15326",
     date: "2025-04-07",
-    status: "Pending",
+    status: "Success",
     gameName: "Game B",
     gameId: "GB002",
     amount: "$15.00"
@@ -22,7 +23,7 @@ const ordersArray = [
   {
     transactionId: "15328",
     date: "2025-04-08",
-    status: "Completed",
+    status: "Success",
     gameName: "Game C",
     gameId: "GC003",
     amount: "$30.00"
@@ -30,7 +31,7 @@ const ordersArray = [
   {
     transactionId: "15329",
     date: "2025-04-09",
-    status: "Failed",
+    status: "Success",
     gameName: "Game D",
     gameId: "GD004",
     amount: "$50.00"
@@ -38,7 +39,7 @@ const ordersArray = [
   {
     transactionId: "15330",
     date: "2025-04-10",
-    status: "Completed",
+    status: "Success",
     gameName: "Game E",
     gameId: "GE005",
     amount: "$25.00"
@@ -46,7 +47,7 @@ const ordersArray = [
   {
     transactionId: "15331",
     date: "2025-04-11",
-    status: "Pending",
+    status: "Success",
     gameName: "Game F",
     gameId: "GF006",
     amount: "$10.00"
@@ -76,37 +77,38 @@ export default function Orders() {
       <div className="card" key={index}>
         <div className="lineOforder">
           <div className="order__column">
-            <p className="order__block"><span>Transaction ID</span></p>
-            <p className="order__block">{`#${order.transactionId}`}</p>
+            <p className="order__title">Transaction ID</p>
+            <p className="order__value">{`#${order.transactionId}`}</p>
           </div>
           <div className="order__column">
-            <p className="order__block"><span>Date</span></p>
-            <p className="order__block">{order.date}</p>
+            <p className="order__title">Date</p>
+            <p className="order__value">{order.date}</p>
           </div>
           <div className="order__column">
-            <p className="order__block"><span>Status</span></p>
-            <p className="order__block">{order.status}</p>
+            <p className="order__title">Status</p>
+            <p className="order__value"><img  style={{ marginRight: "3px"}} src={success} /> {order.status}</p>
           </div>
         </div>
 
         <div className="lineOforder">
           <div className="order__column">
-            <p className="order__block"><span>Game Name</span></p>
-            <p className="order__block">{order.gameName}</p>
+            <p className="order__title">Game Name</p>
+            <p className="order__value">{order.gameName}</p>
           </div>
           <div className="order__column">
-            <p className="order__block"><span>Game Id</span></p>
-            <p className="order__block">{order.gameId}</p>
+            <p className="order__title">Game Id</p>
+            <p className="order__value">{order.gameId}</p>
           </div>
           <div className="order__column">
-            <p className="order__block"><span>Amount</span></p>
-            <p className="order__block">{order.amount}</p>
+            <p className="order__title">Amount</p>
+            <p className="order__value">{order.amount}</p>
           </div>
         </div>
       </div>
     );
   })}
 </main>
+
 
 
       
